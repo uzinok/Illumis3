e42f06256e48bcf5d2a1e1a4e38ac8c807864a17
uzinok <41639106+uzinok@users.noreply.github.com>
2023-06-14 19:01:37 +0300

    Делает кнопки лайка/дислайка

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,47 @@
-console.log('hi!xx');
-const hello = 'hello';
-let bue = 'bue';
-document.write(hello);
-class Buka {
-	constructor(){}
-}
-// alert('hi!!!!!!!!!!!')
+window.addEventListener('load', function() {
+	// const scrollElem = document.querySelectorAll('[data-href]');
+
+	// for (let i = 0; i < scrollElem.length; i++) {
+	// 	scrollElem[i].addEventListener('click', function() {
+	// 		window.location.hash = '#' + this.dataset.href;
+	// 		const element = document.querySelector(`#${this.dataset.href}`);
+	// 		element.scrollIntoView({
+	// 			behavior: "smooth",
+	// 			block: "start",
+	// 			inline: "nearest"
+	// 		});
+	// 	});
+	// }
+
+	const likes = document.querySelectorAll('.like');
+	for (let i = 0; i < likes.length; i++) {
+		likes[i].addEventListener('click', function() {
+			if (this.classList.contains('like--down')) {
+				this.classList.remove('like--down');
+				this.innerText = --this.innerText;
+			} else {
+				this.classList.add('like--down');
+				this.innerText = ++this.innerText;
+
+				if (this.parentNode.querySelector('.dislike--down')) {
+					this.parentNode.querySelector('.dislike--down').classList.toggle('dislike--down')
+				}
+			}
+		});
+	}
+
+	const dislikes = document.querySelectorAll('.dislike');
+	for (let i = 0; i < dislikes.length; i++) {
+		dislikes[i].addEventListener('click', function() {
+			this.classList.toggle('dislike--down');
+			if (this.parentNode.querySelector('.like--down')) {
+				if (this.parentNode.querySelector('.like--down')) {
+					this.parentNode.querySelector('.like--down').innerText = --this.parentNode.querySelector('.like--down').innerText;
+
+					this.parentNode.querySelector('.like--down').classList.toggle('like--down');
+				}
+
+			}
+		});
+	}
+});
